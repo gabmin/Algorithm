37ac7881c6fa60d630e14d902c88d47a74f33bdc
gabmin <rlarkqals@gmail.com>
2021-12-08 17:28:46 +0900

    스택

---
+++ B10828/스택.js
@@ -0,0 +1,55 @@
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+let input = fs.readFileSync(filePath).toString().split("\n");
+
+input.shift();
+const arr = [];
+const response = [];
+for (let i = 0; i < input.length; i++) {
+  switch (input[i]) {
+    case "pop": //마지막 수를 제거하고 그 수의 값을 출력하고 없을 경우 -1을 출력
+      response.push(arr.length === 0 ? -1 : +arr.pop());
+      break;
+
+    case "size": //총 수의 개수를 출력
+      response.push(arr.length);
+      break;
+
+    case "empty": // 수가 없을 경우 1을 출력하고 아닐경우 0을 출력
+      response.push(arr.length === 0 ? 1 : 0);
+      break;
+
+    case "top": // 마지막 수의 값을 출력하고 수가 없을 경우 -1출력
+      response.push(arr.length === 0 ? -1 : +arr[arr.length - 1]);
+      break;
+
+    default:
+      //push는 '명령어 + 수'로 들어오기 때문에 Case가 아닌 방법으로 사용함.
+      arr.push(input[i].split(" ")[1]);
+      break;
+  }
+}
+
+console.log(response.join("\n"));
+
+// class Stack {
+//   constructor() {
+//     this.arr = [];
+//   }
+//   push(X) {
+//     this.arr.push(X);
+//   }
+//   pop() {
+//     this.arr.pop();
+//     return empty() === 1 ? -1 : this.arr[this.arr.length - 1];
+//   }
+//   size() {
+//     return this.arr.length;
+//   }
+//   empty() {
+//     return this.arr.length === 0 ? 1 : 0;
+//   }
+//   top() {
+//     return empty() === 1 ? -1 : this.arr[this.arr.length - 1];
+//   }
+// }
